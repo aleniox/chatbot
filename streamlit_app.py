da81f4de05802368596bea8aba60140455056f44
aleniox <infinity.linhdv@gmail.com>
2024-09-11 23:21:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -220,7 +220,7 @@ def create_vectorstore(data):
 
 def main():
 
-    st.set_page_config(page_title="Abox", page_icon=":speech_balloon:")
+    st.set_page_config(page_title="Chatbot", page_icon=":speech_balloon:")
     st.title("💬 Chatbot")
 
     if "messages" not in st.session_state:
